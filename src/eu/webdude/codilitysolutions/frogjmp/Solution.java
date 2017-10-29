75d15b9a6431053c1df29b5b70a54bed0ed19144
webdude <dimo.petrof@gmail.com>
2017-10-29 16:12:44 +0200

    solved a really easy one

---
+++ src/eu/webdude/codilitysolutions/frogjmp/Solution.java
@@ -0,0 +1,15 @@
+package eu.webdude.codilitysolutions.frogjmp;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10, 85, 30));
+    }
+
+    public int solution(int start, int end, int jumpDistance) {
+        int distanceToCover = Math.abs(end - start);
+        int remainder = distanceToCover % jumpDistance > 0 ? 1 : 0;
+        return (distanceToCover / jumpDistance) + remainder;
+    }
+}
