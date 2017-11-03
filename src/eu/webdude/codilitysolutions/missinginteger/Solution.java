cff3c01bb216fab1085a3a078d17390b73e60a63
webdude <dimo.petrof@gmail.com>
2017-11-03 21:18:05 +0200

    missing integer

---
+++ src/eu/webdude/codilitysolutions/missinginteger/Solution.java
@@ -0,0 +1,27 @@
+package eu.webdude.codilitysolutions.missinginteger;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(new int[]{1, 2, 3});
+        System.out.println(result);
+        int resultAlternate = solution.solution(new int[]{1, 3, 6, 4, 1, 2});
+        System.out.println(resultAlternate);
+    }
+
+    public int solution(int[] input) {
+        Arrays.sort(input);
+        int smallestMissing = 1;
+
+        for (int i : input) {
+            if (i == smallestMissing) {
+                smallestMissing++;
+            }
+        }
+
+        return smallestMissing;
+    }
+}
