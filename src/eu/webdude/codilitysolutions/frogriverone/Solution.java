9c0b5acffd6b1f50b2d8405e5666fc84ecbde41b
webdude <dimo.petrof@gmail.com>
2017-11-03 20:16:09 +0200

    frog rive one - more efficient solution

--- src/eu/webdude/codilitysolutions/frogriverone/Solution.java
+++ src/eu/webdude/codilitysolutions/frogriverone/Solution.java
@@ -1,35 +1,30 @@
 package eu.webdude.codilitysolutions.frogriverone;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int result = solution.solution(5, new int[]{1, 3, 1, 4, 2, 3, 5, 4});
-
         System.out.println(result);
     }
 
     public int solution(int riverLength, int[] leaves) {
         int resultTime = -1;
-        boolean[] river = new boolean[riverLength];
+        Set<Integer> fallenLeaves = new HashSet<>();
 
-        for (int i = 0; i < leaves.length; i++) {
-            river[leaves[i] - 1] = true;
-            if (pathIsClear(river)){
-                return i;
+        for (int second = 0; second < leaves.length; second++) {
+            fallenLeaves.add(leaves[second] - 1);
+
+            boolean pathIsClear = fallenLeaves.size() == riverLength;
+
+            if (pathIsClear) {
+                return second;
             }
         }
 
         return resultTime;
-    }
-
-    private boolean pathIsClear(boolean[] river) {
-        for (boolean b : river) {
-            if (!b) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
