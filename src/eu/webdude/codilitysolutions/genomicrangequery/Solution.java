e814962c9c34424ba145c42bf2fe7048de50af0d
webdude <dimo.petrof@gmail.com>
2017-11-12 10:24:33 +0200

    GenomicRangeQuery works in n+m time

--- src/eu/webdude/codilitysolutions/genomicrangequery/Solution.java
+++ src/eu/webdude/codilitysolutions/genomicrangequery/Solution.java
@@ -1,19 +1,8 @@
 package eu.webdude.codilitysolutions.genomicrangequery;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Solution {
-
-    private static Map<Character, Integer> impactMap = new HashMap<>();
-
-    static {
-        impactMap.put('A', 1);
-        impactMap.put('C', 2);
-        impactMap.put('G', 3);
-        impactMap.put('T', 4);
-    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -22,21 +11,35 @@ public class Solution {
     }
 
     public int[] solution(String input, int[] from, int[] to) {
+        int[][] genoms = new int[3][input.length() + 1];
         int[] result = new int[from.length];
+        int a, c, g;
 
-        for (int i = 0; i < result.length; i++) {
-            result[i] = getSmallestImpact(input, from[i], to[i] + 1);
+        for (int i = 0; i < input.length(); i++) {
+            char chr = input.charAt(i);
+
+            a = 'A' == chr ? 1 : 0;
+            c = 'C' == chr ? 1 : 0;
+            g = 'G' == chr ? 1 : 0;
+
+            genoms[0][i + 1] = genoms[0][i] + a;
+            genoms[1][i + 1] = genoms[1][i] + c;
+            genoms[2][i + 1] = genoms[2][i] + g;
         }
 
+        for (int i = 0; i < from.length; i++) {
+            int fromIndex = from[i];
+            int toIndex = to[i] + 1;
+            if (genoms[0][toIndex] - genoms[0][fromIndex] > 0) {
+                result[i] = 1;
+            } else if (genoms[1][toIndex] - genoms[1][fromIndex] > 0) {
+                result[i] = 2;
+            } else if (genoms[2][toIndex] - genoms[2][fromIndex] > 0) {
+                result[i] = 3;
+            } else {
+                result[i] = 4;
+            }
+        }
         return result;
-    }
-
-    private int getSmallestImpact(String input, int from, int to) {
-        return input
-                .substring(from, to)
-                .chars()
-                .map(x -> impactMap.get((char) x))
-                .min()
-                .orElse(1);
     }
 }
